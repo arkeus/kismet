c250d543648501530c0034b5ae3d2340719904d7
Lee Miller <iarkeus@gmail.com>
2014-09-27 00:03:35 -0700

    Docs

--- src/io/arkeus/kismet/scraper/Scraper.java
+++ src/io/arkeus/kismet/scraper/Scraper.java
@@ -4,6 +4,14 @@ import io.arkeus.kismet.entity.Entity;
 
 import java.util.List;
 
+/**
+ * Interface for a single scraper that knows how to scrape a target and return
+ * the results as a list of entities.
+ */
 public interface Scraper {
+	/**
+	 * Scrapes the target and creates a list of all entities found.
+	 * @return the entities
+	 */
 	public List<Entity> scrape();
 }
